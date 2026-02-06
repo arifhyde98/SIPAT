377f20d725ea5c22316885a941cad3875ab10e1d
arifhyde98 <arif.hyde98@gmail.com>
2026-02-06 20:52:21 +0800

    edit migrate

--- app/Database/Migrations/2026-02-05-000008_ChangeTahunToTanggalPerolehan.php
+++ app/Database/Migrations/2026-02-05-000008_ChangeTahunToTanggalPerolehan.php
@@ -8,13 +8,15 @@ class ChangeTahunToTanggalPerolehan extends Migration
 {
     public function up()
     {
-        $this->forge->addColumn('aset_tanah', [
-            'tanggal_perolehan' => [
-                'type' => 'DATE',
-                'null' => true,
-                'after' => 'harga_perolehan',
-            ],
-        ]);
+        if (! $this->db->fieldExists('tanggal_perolehan', 'aset_tanah')) {
+            $this->forge->addColumn('aset_tanah', [
+                'tanggal_perolehan' => [
+                    'type' => 'DATE',
+                    'null' => true,
+                    'after' => 'harga_perolehan',
+                ],
+            ]);
+        }
 
         if ($this->db->fieldExists('tahun_perolehan', 'aset_tanah')) {
             $this->forge->dropColumn('aset_tanah', 'tahun_perolehan');
