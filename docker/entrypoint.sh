8ace72f9d093fb6f009a192d77b6f439d2054a5a
arifhyde98 <57004703+arifhyde98@users.noreply.github.com>
2026-05-22 13:08:45 +0800

    doclker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,7 @@ if [ ! -d vendor ]; then
     composer install --no-interaction --prefer-dist
 fi
 
-mkdir -p writable/cache writable/debugbar writable/logs writable/session writable/uploads
-chown -R www-data:www-data writable
+mkdir -p writable/cache writable/debugbar writable/logs writable/session writable/uploads public/uploads
+chown -R www-data:www-data writable public/uploads
 
 exec "$@"
